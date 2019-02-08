ff4762c29d7c233c4a06b41172b3b51807f43f9f
Vlad Mocanu <vladmoca90@gmail.com>
2019-02-08 21:19:23 +0000

    Create a triangle with classes

--- scripts/Classes/geometrical-forms.js
+++ scripts/Classes/geometrical-forms.js
@@ -6,4 +6,14 @@ class Polygon {
     }
 }
 
-console.log(new Polygon(4,6).area);
+console.log(new Polygon(4,6).area); //results 24
+
+//A class that contains the perimeter of a triangle
+
+class Triangle {
+    constructor(a, b, c) {
+        this.total = a + b + c;
+    }
+}
+
+console.log(new Triangle(4, 6, 8).total); //results 18
