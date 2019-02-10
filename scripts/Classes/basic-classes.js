8af46371f2111ed1c335a8f2451e908191439bf5
Vlad Mocanu <vladmoca90@gmail.com>
2019-02-10 11:55:33 +0000

    created a class that has a parent

--- scripts/Classes/basic-classes.js
+++ scripts/Classes/basic-classes.js
@@ -7,3 +7,20 @@ class MyName {
 }
 
 console.log(new MyName().name); //results => Vlad Mocanu
+
+//A class that also has a parent. In this case, Polygon is the parent of Square
+
+class Polygon {
+  constructor(height, width) {
+    this.name = 'Polygon';
+    this.height = height;
+    this.width = width;
+  }
+}
+
+class Square extends Polygon {
+  constructor(length) {
+    super(length, length);
+    this.name = 'Square';
+  }
+}
