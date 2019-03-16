60236bc22f9a1a687ea823f47d8720d87eb5691f
Vlad Mocanu <vladmoca90@gmail.com>
2019-03-16 17:31:39 +0000

    Created a class with siblings

--- scripts/Classes/basic-classes.js
+++ scripts/Classes/basic-classes.js
@@ -23,3 +23,19 @@ class Square extends Polygon {
     this.name = 'Square';
   }
 }
+
+//A class that is sibling of another class
+class formatDate extends Date {
+  constructor(dateStr) {
+    super(dateStr);
+  }
+
+  getFormattedDate() {
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    return `${this.getDate()}-${months[this.getMonth()]}-${this.getFullYear()}`;
+  }
+}
+
+console.log(new formatDate('August 19, 1975 23:15:30').getFormattedDate());
