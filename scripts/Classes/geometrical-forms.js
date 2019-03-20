80132037a0ce347dc2cb8564993cedff0da7808c
Vlad Mocanu <vladmoca90@gmail.com>
2019-03-20 21:24:10 +0000

    Created a Square class

--- scripts/Classes/geometrical-forms.js
+++ scripts/Classes/geometrical-forms.js
@@ -5,21 +5,21 @@ class Polygon {
     }
 }
 
-console.log(new Polygon(4,6).area); //results 24
-
-//A class that contains the perimeter of a triangle
-class Triangle {
-    constructor(a, b, c) {
-        this.total = a + b + c;
-    }
-}
-
-console.log(new Triangle(4, 6, 8).total); //results 18
-
 //A class that is sibling of another class
 class Square extends Polygon {
-  constructor(length) {
-    super(length, length);
-    this.name = 'Square';
+  constructor(height, width) {
+    super(height, width);
+  }
+}
+
+let square = new Square(100, 100);
+
+console.log(square); // return {area: 1000}
+
+//A class that is sibling of another class
+class Rectangle extends Polygon {
+  constructor(height, width) {
+    super(height, width);
+    
   }
 }
