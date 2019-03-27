66b721dc3f860014a96b842abbc41c4c3f9bd794
Vlad Mocanu <vladmoca90@gmail.com>
2019-03-27 23:18:25 +0000

    Deleted the test

--- jasmine-tests/utilities/math-utilities.js
+++
@@ -1,9 +0,0 @@
-//A function that trims texts
-
-function trimText(text) {
-    if(text.length == 0) {
-        throw new Error('the text must be given');
-    }
-
-    return text.trim().replace(/\s/g, '');
-}
