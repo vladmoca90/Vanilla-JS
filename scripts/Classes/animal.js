8ab14e0764e5a4caa5cfdbfe57d2964b3a83924a
Vlad Mocanu <vladmoca90@gmail.com>
2019-04-08 23:29:57 +0100

    Created a class Tiger

--- scripts/Classes/animal.js
+++ scripts/Classes/animal.js
@@ -1,5 +1,12 @@
 class Animal {
-    constructor() {
-        
+    constructor(family, species) {
+        this.family = family;
+        this.species = species;
+    }
+}
+
+class Tiger extends Animal {
+    constructor(family, species) {
+        super(family, species);
     }
 }
