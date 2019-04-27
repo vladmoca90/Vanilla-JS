df747f0cf0e550e7a9dc74e457cc653ad12e8172
Vlad Mocanu <vladmoca90@gmail.com>
2019-04-27 23:41:53 +0100

    Started creating the functions

---
+++ scripts/Arrays/index-even.js
@@ -0,0 +1,7 @@
+//A function that removes the elements on an even index
+
+function removeEvenIndexes(list) {
+    if(list.length == 0) {
+        throw new Error('The list cannot be empty');
+    }
+}
---
+++ scripts/Arrays/index-odd.js
@@ -0,0 +1,7 @@
+//A function that removes the elements on an odd index
+
+function removeOddIndexes(list) {
+    if(list.length == 0) {
+        throw new Error('The list cannot be empty');
+    }
+}
