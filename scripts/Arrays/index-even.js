a3486dac6f88c43a9702edee6cf32d20cb2c9b75
Vlad Mocanu <vladmoca90@gmail.com>
2019-04-28 16:59:52 +0100

    Created the code

--- scripts/Arrays/index-even.js
+++ scripts/Arrays/index-even.js
@@ -1,7 +1,17 @@
 //A function that removes the elements on an even index
 
 function removeEvenIndexes(list) {
-    if(list.length == 0) {
+    if (list.length == 0) {
         throw new Error('The list cannot be empty');
     }
+
+    var newList = [];
+
+    for (var i = 0; i < list.length; i++) {
+        if (i % 2 !== 0) {
+            newList.push(list[i]);
+        }
+    }
+
+    return newList;
 }
--- scripts/Arrays/index-odd.js
+++ scripts/Arrays/index-odd.js
@@ -4,4 +4,14 @@ function removeOddIndexes(list) {
     if(list.length == 0) {
         throw new Error('The list cannot be empty');
     }
+
+    var newList = [];
+
+    for (var i = 0; i < list.length; i++) {
+        if (i % 2 == 0) {
+            newList.push(list[i]);
+        }
+    }
+
+    return newList;
 }
