c6640e7d3f2e713901d5a22e806c83ca9a41c3d8
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-21 03:21:21 +0100

    Added a new function

--- scripts/Arrays/randomizeElements.js
+++ scripts/Arrays/randomizeElements.js
@@ -1,14 +1,9 @@
 //A function that changed the order of the elements in an array in random order
 
-functon randomizeElem(list) {
-    if(list.length == 0) {
-        throw new Error('The list cannot be empty');
-    }
-     if(list.length == 1) {
-        return list[0];
-    }
-
-    for (var i = 0; i< list.length; i++) {
-        
-    }
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
 }
+
+let arr = [1, 2, 3];
+shuffle(arr);
+alert(arr);
