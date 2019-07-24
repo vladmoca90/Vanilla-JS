59cdff9e289fd94292acb7e99929feed90d1984c
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-24 23:05:28 +0100

    Renamed again

--- jasmine-tests/utilities/math-utilities.js
+++ jasmine-tests/utilities/math-utilities.js
@@ -3,7 +3,7 @@
 function removeCharacters(text, n) {
 
     if (text.length == 0) {
-        throw new Error('String cannot be empty');
+        throw new Error('Text must exist');
     }
     if(text.length == 1) {
         return text[0];
