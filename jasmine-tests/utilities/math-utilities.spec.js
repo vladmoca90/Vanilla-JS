3ea86979f9c6cea8861ea5bcededdd574e4fbadd
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-24 23:08:31 +0100

    It works now

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -6,15 +6,12 @@ describe('math-utilities', function () {
             }).toThrowError();
         });
         it('returns the only character if the text has only one', function () {
-            expect(function () {
-                removeCharacters('a', 3)
-            }).toBe('a');
+
+            expect(removeCharacters('a', 3)).toBe('a');
         });
         it('returns the number of charcters that you want to remove', function () {
 
-            expect(function() {
-                removeCharacters('abcdefghijk', 3)
-            }).toBe('abc');
+            expect(removeCharacters('abcdefghijk', 3)).toBe('abc');
         });
     });
 });
