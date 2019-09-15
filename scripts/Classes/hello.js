98ab4e047fd1c6e6da915e2f402cc57c8da740a2
Vlad Mocanu <vladmoca90@gmail.com>
2019-09-15 22:54:37 +0100

    Capitalized

--- scripts/Classes/hello.js
+++ scripts/Classes/hello.js
@@ -7,7 +7,7 @@ class Greeting {
 const obj = new Example();
 console.log(obj.hello);
 // "world"
-console.log(Object.getOwnPropertyDescriptor(obj, 'hello'));
+console.log(Object.getOwnPropertyDescriptor(obj, 'Hello'));
 // undefined
-console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(obj), 'hello'));
+console.log(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(obj), 'Hello'));
 // { configurable: true, enumerable: false, get: function get hello() { return 'world'; }, set: undefin
