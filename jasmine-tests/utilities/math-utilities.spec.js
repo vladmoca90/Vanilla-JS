eec23037ca37bb909a8cb1dd7732d3b7b81ca022
Vlad Mocanu <vladmoca90@gmail.com>
2019-10-13 02:39:43 +0100

    The test doesnot work yet

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -2,12 +2,12 @@ describe('math-utilities', function () {
     describe('randomizeElem()', function () {
         it('returns an error if the list is empty', function () {
             expect(function () {
-                randomizeElem([])
+                randomizeElem([ ])
             }).toThrowError();
         });
         it('returns the list if it has only one element', function () {
 
-            expect(randomizeElem([1])).toBe([1]);
+            expect(randomizeElem([0])).toBe([0]);
         });
         it('returns a list with random elements', function () {
 
