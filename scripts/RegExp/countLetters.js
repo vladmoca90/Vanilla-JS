a05ba22845c1d7e84190dbf8f588c8eb9c2b6601
Vlad Mocanu <vladmoca90@gmail.com>
2020-01-26 01:30:50 +0000

    Does not work

---
+++ scripts/RegExp/countLetters.js
@@ -0,0 +1,9 @@
+//A function that counts vowels with RegExp
+
+// function countVowels(sentence) {
+//     if(sentence.length == 0) {
+//         throw new Error('The sentence must be given');
+//     }
+
+//     return sentence.match(/[^aeiou]/gi).length;
+// }
