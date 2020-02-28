19d0b258430f7c91c1f99bf071c3d109a4f5712e
Vlad Mocanu <vladmoca90@gmail.com>
2020-02-28 00:10:50 +0000

    Added a new file with another switch statement of medals

--- scripts/Strings/switch-fruits.js
+++ scripts/Strings/switch-fruits.js
@@ -1,18 +1,18 @@
 let groceryItem = 'papaya';
 
 switch (groceryItem) {
-  case 'tomato':
-    console.log('Tomatoes are $0.49');
-    break;
-  case 'lime':
-    console.log('Limes are $1.49');
-    break;
-  case 'papaya':
-    console.log('Papayas are $1.29');
-    break;
-  default:
-    console.log('Invalid item');
-    break;
+    case 'tomato':
+        console.log('Tomatoes are $0.49');
+        break;
+    case 'lime':
+        console.log('Limes are $1.49');
+        break;
+    case 'papaya':
+        console.log('Papayas are $1.29');
+        break;
+    default:
+        console.log('Invalid item');
+        break;
 }
 
 // Prints 'Papayas are $1.29'
