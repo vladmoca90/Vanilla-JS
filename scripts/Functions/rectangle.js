b3a9ffb38b369e815dc176c5e6a87359a45ded1d
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-03 23:14:11 +0000

    Wrote the same function as arrow function

--- scripts/Functions/rectangle.js
+++ scripts/Functions/rectangle.js
@@ -3,3 +3,9 @@ const rectPerimeter = function(width, height) {
 };
 
 console.log(rectPerimeter(7, 4));
+
+
+//Same function written with arrow functions
+const rectPerimeter = (width, height) => 2*width + 2*height;
+
+console.log(rectPerimeter(12, 8));
