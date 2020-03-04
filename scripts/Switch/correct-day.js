ca3a4325cf554bb018d445076f18e390f4c15ccd
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-04 23:01:03 +0000

    Rewrote the code to be valid format

---
+++ scripts/Switch/correct-day.js
@@ -0,0 +1,24 @@
+function correctDay(day) {
+
+    switch (day) {
+        case 'Monday':
+            console.log('This is the first day of the week!');
+            break;
+        case 'Wednesday':
+            console.log('This is the third day of the week!');
+            break;
+        case 'Sunday':
+            console.log('This is the last day of the week!');
+            break;
+        case 'Tuesday':
+        case 'Thursday':
+        case 'Friday':
+        case 'Saturday':
+            console.log('The day is not what you are looking for.');
+            break;
+        default:
+            throw new Error('The day is not a valid format.');
+    }
+}
+
+correctDay('Sunday');
