a7816a5e2d0e88957f28de26d9748469b161584e
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-05 22:17:02 +0000

    Tested with Jasmine and it works

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,7 +1,7 @@
 describe('math-utilities', function () {
     describe('increaseXbyN()', function () {
         it('returns undefined if n is greater than 6', function () {
-            expect(increaseXbyN(11)).toBe('undefined');
+            expect(increaseXbyN(11)).not.toBeDefined();
         });
           it('returns 6 if n is smaller than 6', function () {
             expect(increaseXbyN(4)).toBe(6);
