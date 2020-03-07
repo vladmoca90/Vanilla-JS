e76991b7825195f65ab39ba09091dd4658a1d5a5
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-07 22:20:53 +0000

    Added conditions

--- scripts/Numbers/allMultiplesUnder.js
+++ scripts/Numbers/allMultiplesUnder.js
@@ -1,3 +1,8 @@
 function returnMultiples(m, n) {
-    
+    if(m <= 0) {
+        throw new Error('The multiple must be positive and greater than 0');
+    }
+    if(n <= 0) {
+        throw new Error('The number must be positive and greater than 0');
+    }
 }
