9a8befba890979445a08d3871d1cb7d2e33c7aaf
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-08 22:12:05 +0000

    Created the same function but with arrow

--- scripts/Numbers/sumOfMultiples.js
+++ scripts/Numbers/sumOfMultiples.js
@@ -10,3 +10,17 @@ function sumOfMultiples(n) {
 }
 
 sumOfMultiples(100);
+
+
+// Second verion of the function
+
+const sumOfMultiples = n => {
+    var sum = 0;
+
+    for (var i = 0; i < n; i++) {
+        if (i % 3 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
