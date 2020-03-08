edec1707eab4d6123058e3038322f3fdf68c391f
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-08 21:54:25 +0000

    Code doesn't work

--- scripts/Numbers/allMultiplesUnder.js
+++ scripts/Numbers/allMultiplesUnder.js
@@ -1,8 +1,17 @@
-function returnMultiples(m, n) {
-    if(m <= 0) {
-        throw new Error('The multiple must be positive and greater than 0');
-    }
+function returnMultiples() {
     if(n <= 0) {
         throw new Error('The number must be positive and greater than 0');
     }
+
+    var sum = 0;
+
+    for(var n = 0; n < 1000; n++) {
+        if(n % 3 == 0) {
+            sum += n;
+        }
+
+        return sum;
+    }
 }
+
+returnMultiples();
