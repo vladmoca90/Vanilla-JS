809a8f7c72ad64aa6695808d6b4f40eb1626e1e5
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-08 22:14:10 +0000

    Added a conditional statement for n

--- scripts/Numbers/sumOfMultiples.js
+++ scripts/Numbers/sumOfMultiples.js
@@ -1,4 +1,8 @@
 function sumOfMultiples(n) {
+    if (n <= 0) {
+        throw new Error('The n must be positive and greater than 0');
+    }
+
     var sum = 0;
 
     for (var i = 0; i < n; i++) {
@@ -13,8 +17,11 @@ sumOfMultiples(100);
 
 
 // Second verion of the function
-
 const sumOfMultiples = n => {
+    if (n <= 0) {
+        throw new Error('The n must be positive and greater than 0');
+    }
+
     var sum = 0;
 
     for (var i = 0; i < n; i++) {
