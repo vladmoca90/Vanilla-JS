7b0f0cb7eb7914a94b130d76cf6137503356b438
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-13 23:43:43 +0000

    Changed the commas

--- scripts/Numbers/sortingNumbers.js
+++ scripts/Numbers/sortingNumbers.js
@@ -1,25 +1,25 @@
 function sortingNUmbers(x, y, x) {
     if(!x || !y || !z) {
-        throw new Error('EVery element must be a valid number');
+        throw new Error('Every element must be a valid number');
     }
 
     if (x > y && x > z) {
         if (y > z) {
-            console.log(x + ", " + y + ", " + z);
+            console.log(x + ', ' + y + ', ' + z);
         } else {
-            console.log(x + ", " + z + ", " + y);
+            console.log(x + ', ' + z + ', ' + y);
         }
     } else if (y > x && y > z) {
         if (x > z) {
-            console.log(y + ", " + x + ", " + z);
+            console.log(y + ', ' + x + ', ' + z);
         } else {
-            console.log(y + ", " + z + ", " + x);
+            console.log(y + ', ' + z + ', ' + x);
         }
     } else if (z > x && z > y) {
         if (x > y) {
-            console.log(z + ", " + x + ", " + y);
+            console.log(z + ', ' + x + ', ' + y);
         } else {
-            console.log(z + ", " + y + ", " + x);
+            console.log(z + ', ' + y + ', ' + x);
         }
     }
 }
