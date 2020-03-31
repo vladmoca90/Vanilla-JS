cdae9a685bdbec2a9b9435f0b733a97d60fefc9a
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-31 22:08:03 +0100

    THe Jasmine code works now. It uses .toEqual()

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,15 +1,18 @@
 describe('math-utilities', function () {
     describe('longestList()', function () {
-        it('returns an error if the lists are equal', function () {
+        it('returns an error if one of the lists is empty', function () {
             expect( function() {
-                longestList([1, 2], [3, 4])
+                longestList([], [3])
             }).toThrowError();
         });
+        it('returns the first list if the have equal length', function () {
+            expect(longestList(['a'], ['c'])).toEqual(['a']);
+        });
         it('returns the first list is this one is longer than the second', function () {
-            expect(longestList([1, 2, 3], ['a'])).toBe([1, 2, 3]);
+            expect(longestList([1, 2, 3], ['a'])).toEqual([1, 2, 3]);
         });
         it('returns the second list if this one is longer than the first', function () {
-            expect(longestList([1], ['a', 'b'])).toBe(['a', 'b']);
+            expect(longestList([1], ['a', 'b'])).toEqual(['a', 'b']);
         });
     });
 });
