a1c0139233fee83cd2023b17a440f5bf370872c5
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-31 21:47:12 +0100

    Deleted spaces

--- jasmine-tests/utilities/math-utilities.js
+++ jasmine-tests/utilities/math-utilities.js
@@ -1,12 +1,14 @@
  //A function that compares the length of two lists and returns the longest one
 
  function longestList(x, y) {
-     if (x.length == y.length) {
-         throw new Error("The lists are equal");
-     } else if (x.length > y.length) {
-         return x;
-     } else {
-         return y;
+    if (x.length == y.length) {
+        throw new Error("The lists are equal");
+    } 
+    else if (x.length > y.length) {
+        return x;
+    } 
+    else {
+        return y;
      }
 
  }
--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,15 +1,15 @@
 describe('math-utilities', function () {
-    describe('nameInitials()', function () {
-        it('returns an error if there is no name', function () {
+    describe('longestList()', function () {
+        it('returns an error if the lists are equal', function () {
             expect( function() {
-                nameInitials('')
+                longestList([1, 2], [3, 4])
             }).toThrowError();
         });
-        it('returns the name is it has only one letter', function () {
-            expect(nameInitials('V')).toBe('V');
+        it('returns the first list is this one is longer than the second', function () {
+            expect(longestList([1, 2, 3], ['a'])).toBe([1, 2, 3]);
         });
-        it('returns the initials if there is a full name', function () {
-            expect(nameInitials('Vlad Mocanu')).toBe('VM');
+        it('returns the second list if this one is longer than the first', function () {
+            expect(longestList([1], ['a', 'b'])).toBe(['a', 'b']);
         });
     });
 });
