5ba2c0b2374426d9208f6b21738104a1edb1dde6
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-31 21:55:45 +0100

    Added a new validation to the function

--- jasmine-tests/utilities/math-utilities.js
+++ jasmine-tests/utilities/math-utilities.js
@@ -1,14 +1,15 @@
  //A function that compares the length of two lists and returns the longest one
 
  function longestList(x, y) {
-    if (x.length == y.length) {
-        throw new Error("The lists are equal");
-    }
-
-    if (x.length > y.length) {
-        return x;
-    } else {
-        return y;
+     if (x.length == 0 || y.length == 0) {
+         throw new Error("None of the lists can be empty");
      }
-
+     if (x.length == y.length) {
+         return x;
+     }
+     if (x.length > y.length) {
+         return x;
+     } else {
+         return y;
+     }
  }
