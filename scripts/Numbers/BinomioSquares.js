d175da4e7f5318685b8106c41e4c8aa428716d91
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-04 00:54:44 +0100

    Added conditions to binomio square

--- scripts/Numbers/BinomioSquares.js
+++ scripts/Numbers/BinomioSquares.js
@@ -1,6 +1,13 @@
 ﻿//Calculate Newton's binome: (a + b)^2
 
 function calculateBinomio(a, b) {
+    if(a == 0) {
+        return b*b;
+    }
+    if(b == 0) {
+        return a*a;
+    }
+
     return (a + b) * (a + b);
 }
 
