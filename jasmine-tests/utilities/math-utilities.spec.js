a50ca7cfec132b9c0198979baf660683d53ea4f3
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-05 20:57:56 +0100

    Created the code but needs fixing

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,12 +1,17 @@
 describe('math-utilities', function () {
-    describe('sumOfMultiples()', function () {
-        it('Returns an error if n is smaller than 0.', function() {
+    describe('degreesToRadians()', function () {
+        it('Returns an error if the degrees number is smaller than 0.', function() {
             expect(function() {
-                sumOfMultiples(-1).toThrowError();
+                degreesToRadians(-90).toThrowError();
             });
         });
-        it('Returns the sum of all multiples of n', function() {
-            expect(sumOfMultiples(8)).toBe(9);
+         it('Returns an error if the degrees number is greater than 360.', function() {
+            expect(function() {
+                degreesToRadians(425).toThrowError();
+            });
+        });
+        it('Returns the degrees converted into radians if the number is between 0 and 360', function() {
+            expect(degreesToRadians(75)).toBe(7.53);
         });
     });
 });
