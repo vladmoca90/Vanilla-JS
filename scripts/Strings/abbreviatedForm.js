c49f888422054cc84a32db7bcbcd9c8dfb2ac483
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-05 23:48:59 +0100

    Made all the text elements abbreviated

--- scripts/Strings/abbreviatedForm.js
+++ scripts/Strings/abbreviatedForm.js
@@ -1,6 +1,5 @@
-//A function that converts a string into abbreviated form
+//A function that converts a string (name) into abbreviated form (ex. Alex Mocanu => A.M.)
 function becomeAbbreviated(text) {
-
     if (text.length == 0) {
         throw new Error('Text cannot be empty');
     }
@@ -11,7 +10,7 @@ function becomeAbbreviated(text) {
     var splitText = text.trim().split(' ');
 
     if(splitText.length > 1) {
-        return splitText[0] + ' ' + splitText[1].charAt(0) + '.';
+        return splitText[0].charAt(0) + '.' + splitText[1].charAt(0) + '.';
     }
 
     return splitText.join();
