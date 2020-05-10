59b22622b29fc22017fdaf869277db774cead2ee
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-10 03:59:06 +0100

    Will review later

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -2,16 +2,16 @@ describe('math-utilities', function () {
     describe('createPerson()', function () {
         it('Returns an error if the list is empty.', function () {
             expect(function () {
-                createPerson('').toThrowError();
+                createPerson([]).toThrowError();
             });
         });
         it('Returns an error if the list has only one element.', function () {
             expect(function () {
-                createPerson('').toThrowError();
+                createPerson(['Vlad Mocanu']).toThrowError();
             });
         });
         it('Returns an object with two properties of the list has two elements.', function () {
-            expect(createPerson('Vlad Mocanu', 25)).toBe("{name: 'Alexandru', age: 25}");
+            expect(createPerson(['Vlad Mocanu', 25])).toBe("{name:'Vlad Mocanu', age: 25}");
         });
     });
 });
--- scripts/Objects/createPerson.js
+++ scripts/Objects/createPerson.js
@@ -1,15 +1,19 @@
 const createPerson = list => {
     if (list.length == 0) {
-        throw new Error('The list cannot be empty');
+        throw new Error('The list cannot be empty.');
+    }
+    if (list.length == 1) {
+        throw new Error('The list needs two elements.');
     }
 
     let person = new Object();
 
     for (let i = 0; i < list.length; i++) {
-        person.name = list[i][0];
-        person.age = list[i][1];
+        person.name = list[0];
+        person.age = list[1];
     }
+
     return person;
 }
 
-createPerson([['Alexandru', 25]]);
+createPerson(['Alexandru', 25]);
