cd864dfbba895d3bd6d2c66cc4e699ccee8408cc
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-10 02:35:54 +0100

    Fixed the code

--- scripts/Objects/createPerson.js
+++ scripts/Objects/createPerson.js
@@ -1,19 +1,15 @@
-//A function the creates an object
-
 const createPerson = list => {
     if (list.length == 0) {
-        throw new Error('The list cannot be empty.');
+        throw new Error('The list cannot be empty');
     }
-    if (list.length == 1) {
-        throw new Error('The list needs two elements.');
-    }
+
+    let person = new Object();
 
     for (let i = 0; i < list.length; i++) {
-        let person = { };
-
         person.name = list[i][0];
         person.age = list[i][1];
     }
-
     return person;
 }
+
+createPerson([['Alexandru', 25]]);
