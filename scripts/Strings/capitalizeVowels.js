dce1d50d6b731d415d83b67fb06b7d226d84fa2b
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-29 23:46:36 +0100

    Replaced var with let

--- scripts/Strings/capitalizeVowels.js
+++ scripts/Strings/capitalizeVowels.js
@@ -1,13 +1,13 @@
 //A function that capitalizes every vowels in a text
 
-function capitalizeVowels(text) {
+const capitalizeVowels = text => {
     if(text.length == 0) {
         throw new error('The text must be given');
     }
 
-    var capVowel = '';
+    let capVowel = '';
 
-    for(var i = 0; i < text.length; i++) {
+    for(let i = 0; i < text.length; i++) {
         if(text.charAt(i) == 'a' || text.charAt(i) == 'e' || text.charAt(i) == 'i' || text.charAt(i) == 'o' || text.charAt(i) == 'u') {
             capVowel += text.charAt(i).toUpperCase();
         } else {
