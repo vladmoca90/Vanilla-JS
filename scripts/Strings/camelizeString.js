b8e25ab48a019a7b32af2eca07ab4b8722868cbc
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-29 23:45:17 +0100

    Replaced var with let

--- scripts/Strings/camelizeString.js
+++ scripts/Strings/camelizeString.js
@@ -1,14 +1,14 @@
 //A function that writes a string as camel case
 
-function camelize(text) {
+const camelize = text => {
     if (text.length == 0) {
         throw new Error('The text cannot be empty');
     }
 
-    var splitText = text.trim().split(' ');
-    var camelizedList = [];
+    let splitText = text.trim().split(' ');
+    let camelizedList = [];
 
-    for (var i = 0; i < splitText.length; i++) {
+    for (let i = 0; i < splitText.length; i++) {
         camelizedList.push(splitText[i].charAt(0).toUpperCase() + splitText[i].substring(1));
     }
 
