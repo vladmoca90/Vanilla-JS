dc333bc1b15c423a10f010f096b93ec21ce50889
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 20:19:46 +0100

    Replaced var with let

--- scripts/Numbers/hammingNumbers.js
+++ scripts/Numbers/hammingNumbers.js
@@ -1,9 +1,9 @@
 //A function that returns the total of Hammning numbers up to 1000 (or any other limit)
 
 const totalOfHamming = (n, m, p) => {
-    var count = 0;
+    let count = 0;
 
-    for (var i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1000; i++) {
         if(i % n == 0 || i % m == 0 || i % p == 0) {
             count++;
         }
