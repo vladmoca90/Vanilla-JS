cd9e0238812a8a792c963ba2eb705c03644dafd2
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 15:33:12 +0100

    Rewrote as an arrow function

--- scripts/Strings/countVowels.js
+++ scripts/Strings/countVowels.js
@@ -1,13 +1,13 @@
 ﻿//write a function that, given a text, returns the number of vowels in the text
-function countVowels(text) {
+const countVowels = text => {
     if (text.length == 0) {
         throw new Error('The text cannot be empty');
     }
 
-    var count = 0;
+    let count = 0;
 
-    for (var i = 0; i < text.length; i++) {
-        var c = text.charAt(i);
+    for (let i = 0; i < text.length; i++) {
+        let c = text.charAt(i);
         if (isVowel(c)) {
             count++;
         }
@@ -16,8 +16,8 @@ function countVowels(text) {
     return count;
 }
 
-function isVowel(c) {
-    var v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+const isVowel = c => {
+    let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
     return v;
 }
