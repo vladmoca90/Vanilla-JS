685c3c24f1db882f54c955651d73371e1b38bef1
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 20:27:17 +0100

    Rewrote as arrow function

--- scripts/Numbers/calculateDigits.js
+++ scripts/Numbers/calculateDigits.js
@@ -1,5 +1,5 @@
 //A function that calculates the addition between the digits of a number
-function digitsAddition(n) {
+const digitsAddition = n => {
     if(!n) {
         throw new Error('The number must be given');
     }
@@ -7,10 +7,10 @@ function digitsAddition(n) {
         throw new Error('The number must be positive');
     }
 
-    var sum = 0;
+    let sum = 0;
 
     while (n > 0) {
-        var digit = n % 10;
+        let digit = n % 10;
         sum += digit;
         n = Math.floor(n / 10)
     }
