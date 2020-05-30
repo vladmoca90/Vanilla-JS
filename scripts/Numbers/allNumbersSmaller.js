c73febf67aa0dcb54c137618a2e1a7120f3d53b6
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 20:29:52 +0100

    Rewrote as arrow function

--- scripts/Numbers/allNumbersSmaller.js
+++ scripts/Numbers/allNumbersSmaller.js
@@ -1,13 +1,13 @@
 //A function that returns a list with all the numbers smaller than a certain number
 
-function allNumbersSmaller(n) {
+const allNumbersSmaller = n => {
     if(n <= 0) {
         throw new Error("The number must be greater than 0");
     }
 
-    var smaller = [];
+    let smaller = [];
 
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         if(n > i) {
             smaller.push(i);
         }
