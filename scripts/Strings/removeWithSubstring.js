bd2cc0b03eefe6caf48b5b87b7bc7f430136b5e0
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 17:20:02 +0100

    Deleted spaces

--- scripts/Strings/removeWithSubstring.js
+++ scripts/Strings/removeWithSubstring.js
@@ -1,7 +1,7 @@
 //A function that removes a certain number of characters from a string
 
 const removeCharacters = (text, n) => {
-    if (text.length == 0) {
+    if(text.length == 0) {
         throw new Error('String cannot be empty');
     }
     if(text.length == 1) {
