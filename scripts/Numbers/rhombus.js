ae6575a06e0a3f9325db7bf9f1c7e89d0ba9501f
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 20:17:32 +0100

    Renamed the function that calculates the rhombus area

--- scripts/Numbers/rhombus.js
+++ scripts/Numbers/rhombus.js
@@ -1,6 +1,6 @@
 //A function that calculates area and other things from rhombus
 
-const calcArea = (base, height) => {
+const calcAreaRhombus = (base, height) => {
     if(base == 0 || height == 0) {
         throw new Error('The shape is not a rhombus.');
     }
