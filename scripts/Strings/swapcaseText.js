f9713e6d570fb0590118400885d99da8ba44d177
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 17:35:59 +0100

    Rewrote as arrow function

--- scripts/Strings/swapcaseText.js
+++ scripts/Strings/swapcaseText.js
@@ -1,13 +1,13 @@
 //A function that makes every uppercase letter of a text as lowercase and every lowercase as uppercase
 
-function swapcaseText(text) {
+const swapcaseText = text => {
     if(text.length == 0) {
         throw new Error('The text must be given');
     }
 
-    var res = '';
+    let res = '';
 
-    for(var i = 0; i < text.length; i++) {
+    for(let i = 0; i < text.length; i++) {
         if(text.charAt(i) == text.charAt(i).toLowerCase()) {
             res += text.charAt(i).toUpperCase();
         }
