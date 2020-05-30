179782acec5659ecb1d4e04df438ade9f20e6ad5
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 17:37:34 +0100

    Rewrote as arrow function

--- scripts/Strings/vowelsAndConsonants.js
+++ scripts/Strings/vowelsAndConsonants.js
@@ -1,15 +1,15 @@
 //A function that return a json object with vowels and consonants or a text
 
-function showVowelsAndConsonants(text) {
+const showVowelsAndConsonants = text => {
     if(text.length == 0) {
         throw new Error('The text must be given');
     }
 
-    var letters = {}
-    var vowels = [];
-    var consonants = [];
+    let letters = {}
+    let vowels = [];
+    let consonants = [];
 
-    for (var i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
         if(charAt(i) == isVowel()) {
             vowels.push(text[i]);
         }
