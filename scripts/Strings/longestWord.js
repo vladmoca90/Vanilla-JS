54cf5afb7af5691be0f7df78c1af666aeb06ef50
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-30 15:35:03 +0100

    Rewrote as arrow function

--- scripts/Strings/longestWord.js
+++ scripts/Strings/longestWord.js
@@ -1,14 +1,14 @@
 //A function that returns the longest word in a text
 
-function longestWord(text) {
+const longestWord = text => {
     if (text.length == 0) {
         throw new Error('The text cannot be empty');
     }
 
-    var splitText = text.trim().split(' ');
-    var word = splitText[0];
+    let splitText = text.trim().split(' ');
+    let word = splitText[0];
 
-    for (var i = 0; i < splitText.length; i++) {
+    for (let i = 0; i < splitText.length; i++) {
         if(word.length < splitText[i].length) {
             word = splitText[i];
         }
