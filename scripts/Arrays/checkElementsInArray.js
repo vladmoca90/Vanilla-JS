9baa8b99361a0918969d02265bd83f2d32b68368
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-31 22:32:58 +0100

    Rewrote as arrow function

--- scripts/Arrays/checkElementsInArray.js
+++ scripts/Arrays/checkElementsInArray.js
@@ -1,11 +1,11 @@
 //A function that checks if all elements in an list fulfill a certain condition
 
-function checkElementsInList(list) {
+const checkElementsInList = list => {
     if(list.length == 0) {
         throw new Error('The list cannot be empty');
     }
 
-    for (var i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length; i++) {
         if(list[i] > 10) {
             return false;
         }
