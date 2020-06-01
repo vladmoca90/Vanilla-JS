5471d44fc91dea817ba34cb3c14077ac182a7539
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-01 23:27:14 +0100

    Rewrote as arrow function

--- scripts/Arrays/filterArray.js
+++ scripts/Arrays/filterArray.js
@@ -1,12 +1,12 @@
 //A function that filters an list
-function filterList(list) {
+const filterList = list => {
     if(list.lenth == 0) {
         throw new Error('The list cannot be empty');
     }
 
-    for (var i = 0; i < list.length; i++) {
-        var result = list.filter(i => list.length > 6);
-    }
+    for (let i = 0; i < list.length; i++) {
+        let result = list.filter(i => list.length > 6);
 
-    return result;
+        return result;
+    }
 }
