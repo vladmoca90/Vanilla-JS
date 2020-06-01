ef4b1263a1a3611deb20b61188d556d26d8a9c94
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-01 23:26:00 +0100

    Rewrote as arrow function

--- scripts/Arrays/elemLengthMap.js
+++ scripts/Arrays/elemLengthMap.js
@@ -1,6 +1,6 @@
 //A function that returns an array with the length of elements
 
-function returnElemLength(elements) {
+const returnElemLength = elements => {
     if(elements.length == 0) {
         throw new Error('The list cannot be empty');
     }
