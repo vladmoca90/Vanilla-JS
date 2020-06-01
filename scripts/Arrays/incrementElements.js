673e1453f52df959c642ad2c69d731b9745fa804
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-01 23:51:01 +0100

    Rewrote as arrow function

--- scripts/Arrays/incrementElements.js
+++ scripts/Arrays/incrementElements.js
@@ -1,11 +1,11 @@
 //A function that increments the elements in an list by n
 
-function incrementElements(list, n) {
+const incrementElements = (list, n) => {
     if(list.length == 0) {
         throw new Error('The list cannot be empty');
     }
 
-    for (var i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length; i++) {
         list[i] += n;
     }
 
