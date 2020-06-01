6918449dc77af3400ac0bc40fc11b1b7acde8eb8
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-01 23:53:38 +0100

    Rewrote as arrow function

--- scripts/Arrays/longestList.js
+++ scripts/Arrays/longestList.js
@@ -1,6 +1,6 @@
  //A function that compares the length of two lists and returns the longest one
 
- function longestList(x, y) {
+const longestList = (x, y) => {
      if(x.length == 0 || y.length == 0) {
          throw new Error("None of the lists can be empty");
      }
