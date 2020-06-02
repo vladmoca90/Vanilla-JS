a7369671e986c250d5159bad48c32ea124989c7a
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-02 22:55:39 +0100

    Rewrote as arrow function

--- scripts/Arrays/removeUnwantedValues.js
+++ scripts/Arrays/removeUnwantedValues.js
@@ -1,13 +1,13 @@
-//A function that removes null, undefined, ' ', 0, NaN values from an array
+//A const that removes null, undefined, ' ', 0, NaN values from an array
 
-function removeUnwantedValues(list) {
+const removeUnwantedValues = list => {
     if(list.length == 0) {
         throw new Error('The list cannot be empty');
     }
 
-    var newList = [];
+    let newList = [];
 
-    for (var i = 0; i < list.length; i++) {
+    for (let i = 0; i < list.length; i++) {
         if(list[i] !== 'undefined' || list[i] !== 0 || list[i] !== null || list[i] !== ' ' || list[i] !== NaN) {
             newList.push(list[i]);
         }
