4fb27646e811b96dd67a625cd4abd77ca4cd68d9
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-02 23:14:31 +0100

    Rewrote as arrow function

--- scripts/Arrays/someSmallerThan.js
+++ scripts/Arrays/someSmallerThan.js
@@ -1,6 +1,6 @@
 //Returns true is at least one element in an array in smaller than a given number
 
-function compareNumbersWith(list, n) {
+const compareNumbersWith = (list, n) => {
     if(list.length == 0) {
         throw new Error('the list cannot be empty');
     }
