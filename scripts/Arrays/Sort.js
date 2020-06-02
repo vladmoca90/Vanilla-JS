18e2e21b1aaab97b2019496146fc91c4d0b317cb
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-02 23:19:24 +0100

    Deleted 2 files

--- scripts/Arrays/Sort.js
+++
@@ -1,9 +0,0 @@
-﻿// var names = ["Elliot", "Joana", "Vlad", "Sarah", "Alexander", "Katherine"];
-
-// document.getElementById("sorting").innerHTML = names;
-
-// function sortingNames() {
-
-//     document.getElementById("sorting").innerHTML = names.sort();
-
-// }
