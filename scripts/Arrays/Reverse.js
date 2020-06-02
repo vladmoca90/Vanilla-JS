c0b9baddb9542c489d6a644fa49e6c410f551e72
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-02 23:08:33 +0100

    Deleted

--- scripts/Arrays/Reverse.js
+++
@@ -1,9 +0,0 @@
-﻿// var names = ["Elliot", "Joana", "Vlad", "Sarah", "Alexander", "Katherine"];
-
-// document.getElementById("reversing").innerHTML = names;
-
-// function reverseNames() {
-
-//     document.getElementById("reversing").innerHTML = names.reverse();
-
-// }
