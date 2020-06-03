5ff42828e9cc8fad0610e9b095ec2a3bfc9c3d3f
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-03 23:50:34 +0100

    Rewrote as arrow function

--- scripts/Objects/daysUntilChristmas.js
+++ scripts/Objects/daysUntilChristmas.js
@@ -1,7 +1,7 @@
 //A function that return how many days are left until Christmas
 
-function returnDaysUntilChristmas(today) {
-    var today = new Date();
+const returnDaysUntilChristmas = today => {
+    let today = new Date();
 
     if(today.getMonth() == 11) {
         if(today.getDate() < 25) {
@@ -14,10 +14,10 @@ function returnDaysUntilChristmas(today) {
         return 31 - today.getDate() + 359;
     }
 
-    var daysLeft = 24;
-    var daysOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let daysLeft = 24;
+    let daysOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    for (var i = today.getMonth() + 1; daysOfTheMonth.length - 1; i++) {
+    for (let i = today.getMonth() + 1; daysOfTheMonth.length - 1; i++) {
         daysLeft += daysOfTheMonth[i] + (daysOfTheMonth[today.getMonth()] - today.getDate());
 
         if(daysOfTheMonth[1] == 29 && today.getMonth() == 0 || today.getMonth() == 1) {
