3ec2822d8f55c6fd9e57553f3990922b62dffc2b
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-04 23:49:19 +0100

    Made the Jasmine code general

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,17 +1,17 @@
 describe('math-utilities', function () {
-    describe('createPerson()', function () {
-        it('Returns an error if the list is empty.', function () {
+    describe('', function () {
+        it('', function () {
             expect(function () {
-                createPerson([]).toThrowError();
+
             });
         });
-        it('Returns an error if the list has only one element.', function () {
+        it('', function () {
             expect(function () {
-                createPerson(['Vlad Mocanu']).toThrowError();
+          
             });
         });
-        it('Returns an object with two properties of the list has two elements.', function () {
-            expect(createPerson(['Vlad Mocanu', 25])).toBe("{name:'Vlad Mocanu', age: 25}");
+        it('', function () {
+
         });
     });
 });
