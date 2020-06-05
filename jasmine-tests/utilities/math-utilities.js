189dab15feda07603e25278f3d230f45c5566c2d
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-05 23:03:00 +0100

    Will test the threeEvenNum() function

---
+++ jasmine-tests/utilities/math-utilities.js
@@ -0,0 +1,9 @@
+//A function that returns true if all three given numbers are even
+
+const threeEvenNum = (a, b, c) => {
+    if(a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
+        return false;
+    }
+
+    return true;
+}
