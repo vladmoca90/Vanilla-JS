2eeadc463882b4a2781db21c634964bd0d784aa1
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-05 23:05:23 +0100

    Created the Jasmine test

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,17 +1,14 @@
 describe('math-utilities', function () {
-    describe('', function () {
-        it('', function () {
+    describe('threeEVenNum()', function () {
+        it('returns true if all three numbers are even.', function () {
             expect(function () {
-
+                threeEvenNum(2, 4, 6).toBeTruthy()
             });
         });
-        it('', function () {
+        it('returns falseif at least of of the numbers is not even.', function () {
             expect(function () {
-          
+                threeEvenNum(2, 5, 6).toBeFalsy()
             });
-        });
-        it('', function () {
-
         });
     });
 });
