1a64fb31558947e863cf5466dc6f51fcad1ed6df
Vlad Mocanu <vladmoca90@gmail.com>
2020-07-21 23:45:21 +0100

    Deleted the code from the testing files

--- jasmine-tests/utilities/math-utilities.js
+++
@@ -1,9 +0,0 @@
-//A function that returns true if all three given numbers are even
-
-const threeEvenNum = (a, b, c) => {
-    if(a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0) {
-        return false;
-    }
-
-    return true;
-}
