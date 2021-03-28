b9ec54891e556db20107f9321619d9ada5b68369
Vlad Mocanu <vladmoca90@gmail.com>
2021-03-28 16:58:01 +0100

    Deleted spaces

--- scripts/Arrays/randomizeElements.js
+++ scripts/Arrays/randomizeElements.js
@@ -1,9 +1,10 @@
 //A function that changed the order of the elements in an list in random order
+
 const randomizeElem = list => {
   if(list.length == 0) {
     throw new Error('The list cannot be empty');
   }
-   if(list.length == 1) {
+  if(list.length == 1) {
     return list[0];
   }
 
