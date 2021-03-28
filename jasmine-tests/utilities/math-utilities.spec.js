87f0245ac8f548d99bdd410da5bbc20420e3fedd
Vlad Mocanu <vladmoca90@gmail.com>
2021-03-28 17:03:19 +0100

    Deleted the function from the Jasmine code

--- jasmine-tests/utilities/math-utilities.spec.js
+++ jasmine-tests/utilities/math-utilities.spec.js
@@ -1,13 +1,13 @@
 describe('math-utilities', function () {
-    describe('threeEVenNum()', function () {
-        it('returns true if all three numbers are even.', function () {
+    describe('', function () {
+        it('', function () {
             expect(function () {
-                threeEvenNum(2, 4, 6).toBeTruthy()
+                
             });
         });
-        it('returns false if at least of of the numbers is not even.', function () {
+        it('', function () {
             expect(function () {
-                threeEvenNum(2, 5, 6).toBeFalsy()
+               
             });
         });
     });
