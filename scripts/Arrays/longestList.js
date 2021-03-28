39fb68266492bb34a5cad23f91a9508fdf70edfe
Vlad Mocanu <vladmoca90@gmail.com>
2021-03-28 16:59:48 +0100

    Aligned the code

--- scripts/Arrays/longestList.js
+++ scripts/Arrays/longestList.js
@@ -1,15 +1,15 @@
- //A function that compares the length of two lists and returns the longest one
+//A function that compares the length of two lists and returns the longest one
 
 const longestList = (x, y) => {
-     if(x.length == 0 || y.length == 0) {
-         throw new Error("None of the lists can be empty");
-     }
-     if(x.length == y.length) {
-         return x;
-     }
-     if(x.length > y.length) {
-         return x;
-     } else {
-         return y;
-     }
- }
+    if(x.length == 0 || y.length == 0) {
+        throw new Error("None of the lists can be empty");
+    }
+    if(x.length == y.length) {
+        return x;
+    }
+    if(x.length > y.length) {
+        return x;
+    } else {
+        return y;
+    }
+}
