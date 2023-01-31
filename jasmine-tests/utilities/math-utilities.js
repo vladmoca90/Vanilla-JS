50245a659387783c0c71f158cce45d3ea8f3e01b
Vlad Mocanu <vladmoca90@gmail.com>
2023-01-31 14:56:46 +0000

    Going to do a Jasmine test

---
+++ jasmine-tests/utilities/math-utilities.js
@@ -0,0 +1,11 @@
+//Returns true is at least one element in an array in smaller than a given number
+
+const compareNumbersWith = (list, n) => {
+    if(list.length == 0) {
+        throw new Error('the list cannot be empty');
+    }
+
+    return list.some(function(elem) {
+        return elem > n;
+    });
+}
