d44020681830b772b664e27641adb092e197fe05
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-21 00:24:50 +0100

    Testing a simple function using .find()

---
+++ scripts/Arrays/findElemInArray.js
@@ -0,0 +1,11 @@
+//A function that find certain element/elements in a given array
+
+const findElem = (list, n) => {
+    if(list.length == 0) {
+        throw new Error('The list cannot be empty');
+    }
+
+    for (let i = 0; i<list.length; i++) {
+
+    }
+}
