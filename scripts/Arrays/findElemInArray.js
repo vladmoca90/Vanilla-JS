466935fca21025aaa0d84d2410456f9f74031b52
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-21 00:42:26 +0100

    Will fix tomorrow

--- scripts/Arrays/findElemInArray.js
+++ scripts/Arrays/findElemInArray.js
@@ -5,15 +5,11 @@ const findElem = (list) => {
         throw new Error('The list cannot be empty');
     }
 
-    let foundList = [];
+   /* let foundList = []
 
     for (let i = 0; i < list.length; i++) {
-        foundList = list.find(list[i] > 10);
-
-        if (list[i].length > 10) {
-            return foundList;
-        } else {
-            throw new Error('The text does not have more than 10 characters');
-        }
+       foundList.push(list[i].find(i => i > 7));
     }
+
+    return foundList; */
 }
