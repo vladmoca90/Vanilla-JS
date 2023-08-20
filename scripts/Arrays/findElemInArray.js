6e3f68a8bc07fc97418be4be77c0e28d8ef0c469
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-21 00:29:00 +0100

    Created the function. Now will test it.

--- scripts/Arrays/findElemInArray.js
+++ scripts/Arrays/findElemInArray.js
@@ -1,11 +1,19 @@
 //A function that find certain element/elements in a given array
 
 const findElem = (list, text) => {
-    if(list.length == 0) {
+    if (list.length == 0) {
         throw new Error('The list cannot be empty');
     }
 
-    for (let i = 0; i<list.length; i++) {
-        list.find(text)
+    let foundList = [];
+
+    for (let i = 0; i < list.length; i++) {
+        foundList = list.find(text.length > 10);
+
+        if (text.length > 10) {
+            return foundList;
+        } else {
+            throw new Error('The text does not have more than 10 characters');
+        }
     }
 }
