4b4906f5d8b11e9b8508ef02adc94b8220ef622b
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-23 22:21:16 +0100

    Creating a project where you destructure an object

---
+++ scripts/Objects/destructuring.js
@@ -0,0 +1,13 @@
+//A file that shows how to destructure an object
+const person = {
+    name: 'Vlad Mocanu',
+    age: 33,
+    isMarried: false,
+}
+
+//old style
+const name = person.name;
+const age = person.age;
+const isMarried = person.isMarried;
+
+//destructure
