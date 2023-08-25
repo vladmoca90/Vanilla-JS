22a4f2d1143edbb582545e8fd0e4b0d6b82658b5
Vlad Mocanu <vladmoca90@gmail.com>
2023-08-25 23:23:23 +0100

    Uncommented some writing variants. Ignore the errors!

--- scripts/Objects/destructuring.js
+++ scripts/Objects/destructuring.js
@@ -1,5 +1,4 @@
 //A file that shows how to destructure an object
-
 const person = {
     name: 'Vlad Mocanu',
     age: 33,
@@ -7,31 +6,31 @@ const person = {
 }
 
 //old style
-/*
   const name = person.name;
   const age = person.age;
   const isMarried = person.isMarried; 
-*/
+
 
 //one option
-
-/*  const {name, age, isMarried} = person;  */
+const {name, age, isMarried} = person;
 
 //another option
 const name = 'Vlad Mocanu';
 const age = 33;
 const isMarried = false;
 
-const person = { //another way to write the object - ignore the error!
+//another way to write the object - ignore the error!
+const person = {
     name,
     age,
     isMarried,
 }
 
+//an object exact as person = {} but only the name property has a different value
 const person2 = {
     ...person,
     name: 'Ana Mocanu',
-} //an object exact as person = {} but only the name property has a different value
+}
 
 //a way to destructure arrays
 const names = ['Pedro', 'Jack', 'Jessica'];
