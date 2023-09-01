06dd22ebe6d2ccdd006bd5c198e63160f9e25282
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-01 22:39:56 +0100

    Added a different definition for .reduce()

--- scripts/Arrays/reduceArray.js
+++ scripts/Arrays/reduceArray.js
@@ -1,10 +1,14 @@
-//The reduce() method executes a reducer function on each element of the array and returns a single output value.
+//The .reduce() method in JavaScript is used to reduce the array to a single value and executes a provided function 
+//for each value of the array (from left to right) and the return value of the function is stored in an accumulator. 
+
 //Method is array.reduce(callback(accumulator, currentValue), initialValue)
 
 const numbers = [1800, 50, 300, 20, 100];
 
 let difference = numbers.reduce(
-    (accumulator, currentValue) => accumulator - currentValue
+    (accumulator, currentValue) => accumulator - currentValue 
+    // 1800 - 50 - 300 - 20 - 100 (the first element is the accumulator in this case)
+    // currentValue represents all elements from the array
 );
 
 console.log(difference); // The result will be 1330
