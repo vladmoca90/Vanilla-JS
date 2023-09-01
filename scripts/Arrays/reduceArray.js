edfc319e01184c06e3c4c8a538f6071727b3fb06
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-01 23:52:01 +0100

    Added another example with the .reduce()

--- scripts/Arrays/reduceArray.js
+++ scripts/Arrays/reduceArray.js
@@ -2,15 +2,35 @@
 //for each value of the array (from left to right) and the return value of the function is stored in an accumulator. 
 
 //General form is like this:
-array.reduce(callbackFn, [initialValue]); //callbackFn is called reducer and the initialValue is an optional one
+//array.reduce(callbackFn, initialValue); //callbackFn is called reducer and the initialValue is an optional one
 
 //Example
-const numbers = [1800, 50, 300, 20, 100];
+const num = [1800, 50, 300, 20, 100];
 
-let difference = numbers.reduce(
+let difference = num.reduce(
     (accumulator, currentValue) => accumulator - currentValue
     // 1800 - 50 - 300 - 20 - 100 (the first element is the accumulator in this case)
     // currentValue represents all elements from the array
 );
 
 console.log(difference); // The result will be 1330
+
+
+
+//Another example, but without .reduce()
+const numbers = [1, -1, 2, 3];
+let sum = 0;
+
+for( let n of numbers) {
+    sum += n;
+}
+
+console.log(sum); //will display 5.
+
+//Same example with .reduce()
+
+const total = numbers.reduce((accumulator, currentValue) => { //accumulator is like the above sum variable
+    return accumulator + currentValue
+}, 0); //The 0 value is initial value for the accumulator
+
+console.log(total);
