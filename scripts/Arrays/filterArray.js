8b4ec73cfb5abdb43252d37095176b80ec703429
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-04 22:13:46 +0100

    Deleted the initial function

--- scripts/Arrays/filterArray.js
+++ scripts/Arrays/filterArray.js
@@ -1,22 +1,12 @@
 //A function that filters an list according to a certain condition.
-
-const filterList = list => {
-    for (let i = 0; i < list.length; i++) {
-        let result = list.filter(i => list.length > 6);
-
-        return result;
-    }
-}
-
-
-const names = ['Ana', 'Vlad', 'Alex', 'James', 'Thomas'];
+const names = ['Ana', 'Vlad', 'Alex', 'James', 'Thomas', 'Fiona', 'Anastasia', 'Zachary', 'Ian'];
 
 const filterNames = names => {
-    for (let i = 0; i < names.length; i++) {
-        names.filter((name) => {
-            return name.startsWith('A');
-        });
-    }
+    let filteredName = names.filter((name) => {
+        return name.charAt() === 'A';
+    });
+
+    return filteredName;
 }
 
 console.log(filterNames(names));
