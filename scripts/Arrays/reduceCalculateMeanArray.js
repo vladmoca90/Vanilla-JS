752693d3e20b39fa3aebe218d09615e04095ff87
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-04 21:15:44 +0100

    Created a function with .reduce() that calculates the average of an array of numbers.

---
+++ scripts/Arrays/reduceCalculateMeanArray.js
@@ -0,0 +1,23 @@
+//A function that calculates the mean of the numbers in an array using .reduce()
+
+const numbers = [1, 2, 4, 7, 9, 15, 16];
+
+//Method 1
+const averageNumb = () => {
+    const sum = numbers.reduce((accumulator, number) => {
+        accumulator += number;
+        return accumulator;
+    }, 0); //classical way of writing the calculus.
+    
+    //the accumulator is 0 (the second argument) and to this number we will add each number in the array. 
+    //just like creating let sum = 0; without using .reduce()
+
+    return (sum/numbers.length); //will be 7.714285714285714 -> use .toFixed() for example to have only 7.71
+}
+
+//Method 2
+const averageNumb2 = () => {
+     const sum = numbers.reduce((accumulator, number) => accumulator + number);
+ 
+     return (sum/numbers.length); //will be 7.714285714285714 as above
+ }
