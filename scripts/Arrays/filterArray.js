e2a3e01d0187e6bdf73ae33f899a3c76fc6577e0
Vlad Mocanu <vladmoca90@gmail.com>
2023-09-04 21:44:23 +0100

    Deleted the condition for .length == 0 as .filter() does not execute it anyway

--- scripts/Arrays/filterArray.js
+++ scripts/Arrays/filterArray.js
@@ -1,13 +1,22 @@
 //A function that filters an list according to a certain condition.
 
 const filterList = list => {
-    if(list.length == 0) {
-        throw new Error('The list cannot be empty');
-    }
-
     for (let i = 0; i < list.length; i++) {
         let result = list.filter(i => list.length > 6);
 
         return result;
     }
 }
+
+
+const names = ['Ana', 'Vlad', 'Alex', 'James', 'Thomas'];
+
+const filterNames = names => {
+    for (let i = 0; i < names.length; i++) {
+        names.filter((name) => {
+            return name.startsWith('A');
+        });
+    }
+}
+
+console.log(filterNames(names));
