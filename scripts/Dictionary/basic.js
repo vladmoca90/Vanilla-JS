842835fd9d4b174be4d87e6e653edbc5adf12f24
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-29 20:26:51 +0000

    Started to write dictionary examples

---
+++ scripts/Dictionary/basic.js
@@ -0,0 +1,14 @@
+//Basic rules for dictionaries
+
+// You can use the build-in Object, such as newObject(); or the literal notation {};
+
+let animalDict = {
+    'bird': 'goose',
+    'fish': 'shark',
+    'insect': 'spider',
+    'mammal': 'wolf',
+    'reptile': 'monitor',
+}
+
+animalDict['bird'] = 'goose'; 
+animalDict.bird = 'goose';
