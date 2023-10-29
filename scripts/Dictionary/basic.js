b213a23aafec5d410465b8e2197e057e3fbac54b
Vlad Mocanu <vladmoca90@gmail.com>
2023-10-29 23:37:38 +0000

    Added extra documentation especially on enums, unions or other more complex data.

--- scripts/Dictionary/basic.js
+++ scripts/Dictionary/basic.js
@@ -1,5 +1,7 @@
 //Basic rules for dictionaries.
 
+// Dictionaries are objects that have keys and value. --> const dictionary[key] = value;
+
 // You can use the build-in Object, such as newObject(); or the literal notation {};
 
 let animalDict = {
@@ -15,4 +17,11 @@ animalDict.bird = 'goose'; //accessing the property by dot.
 
 //When using Typescript make sure you write the data type as well. --> const dictionary: any = {}; (example)
 
-//A dictionary can have a key and a value. --> const dictionary
+//const dictionary: { [key: string]: string } = {}; (exact type of data)
+
+//you can also use .map() on dictionaries --> const dictionary = new Map<string, number>()
+
+//if you use dictionaries on union, enums or more complex data use the Record<Keys, Type> notation.
+
+//example you have type JobInfo = { ... }  and type JobPosition = { ... }
+//they can be included in a dictionary as const dictionary = new Map<JobInfo, JobPosition>()
