c7b7a645ed6b8ae220d8eef7877a22208398ce5e
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-29 22:05:41 +0100

    Improved add to object projects

--- scripts/Objects/addToObject/country.js
+++ scripts/Objects/addToObject/country.js
@@ -1,17 +1,49 @@
-// A object with properties of a country
+// An object representing a country and its properties.
 
 const country = {
     name: "Uzbekistan",
-    population: 27, // in millions
+    population: 27, // Population in millions
     capital: "Tashkent",
-    otherCities: ["Samarkhand", "Burkhara", "Nukus"],
-}
+    otherCities: ["Samarkand", "Bukhara", "Nukus"],
+};
 
-const addToCountry = country => {
+// Adds additional properties to a country object.
+const addToCountry = (country) => {
+    // Validate the object
+    if (!country) {
+        throw new Error("A country object is required.");
+    }
+
     return Object.assign(country, {
-        size: 447400, // in square km
+        size: 447400, // Area in square kilometres
         region: "Central Asia",
         continent: "Asia",
         independence: 1991,
     });
-}
+};
+
+// Update the country object
+const updatedCountry = addToCountry(country);
+
+// Display the updated object
+console.log(updatedCountry);
+
+/*
+Theory
+
+- Objects can be modified after they are created by adding
+  new properties.
+
+- Object.assign(target, source) copies the properties from
+  the source object into the target object.
+
+- The first argument is the object to update.
+- The second argument contains the new properties.
+
+Syntax
+
+Object.assign(targetObject, {
+    property1: value1,
+    property2: value2
+});
+*/
--- scripts/Objects/addToObject/student.js
+++ scripts/Objects/addToObject/student.js
@@ -1,14 +1,18 @@
-// A function that assigns values to an object
+// A student object
 
 const student = {
     name: "Angelika Kloc",
     age: 25,
-    nationality: "Polish"
+    nationality: "Polish",
 };
 
-const addToStudent = student => {
+// A function that adds new properties to the student object
+
+const addToStudent = (student) => {
     return Object.assign(student, {
         grade: 10,
         pastConvictions: false,
     });
-}
+};
+
+console.log(addToStudent(student));
