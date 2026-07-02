687cbe08aa1c4cb2c8e2e1778cde7f940b741f6d
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-02 21:29:56 +0100

    Corrected the daysUntilChristmas code

--- scripts/Others/daysUntilChristmas.js
+++ scripts/Others/daysUntilChristmas.js
@@ -1,29 +1,34 @@
-// A function that return how many days are left until Christmas
+// Returns how many days are left until Christmas.
 
-const returnDaysUntilChristmas = today => {
-    let today = new Date();
+const returnDaysUntilChristmas = () => {
+  const today = new Date();
 
-    if (today.getMonth() == 11) {
-        if (today.getDate() < 25) {
-            return 25 - today.getDate();
-        }
-        if (today.getDate() == 25) {
-            return 0;
-        }
+  const isLeapYear =
+    (today.getFullYear() % 4 === 0 && today.getFullYear() % 100 !== 0) ||
+    today.getFullYear() % 400 === 0;
 
-        return 31 - today.getDate() + 359;
-    }
+  const daysOfTheMonth = [
+    31,
+    isLeapYear ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+  ];
 
-    let daysLeft = 24;
-    let daysOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let daysLeft = 25 - today.getDate();
 
-    for (let i = today.getMonth() + 1; daysOfTheMonth.length - 1; i++) {
-        daysLeft += daysOfTheMonth[i] + (daysOfTheMonth[today.getMonth()] - today.getDate());
+  for (let i = today.getMonth() + 1; i < 11; i++) {
+    daysLeft += daysOfTheMonth[i];
+  }
 
-        if (daysOfTheMonth[1] == 29 && today.getMonth() == 0 || today.getMonth() == 1) {
-            daysLeft += daysOfTheMonth[i] + (daysOfTheMonth[today.getMonth()] - today.getDate()) + 1;
-        }
-    }
+  return daysLeft;
+};
 
-    return daysLeft;
-}
+console.log(returnDaysUntilChristmas());
