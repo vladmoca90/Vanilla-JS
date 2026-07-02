bf5ab56723fbf76b5534537586cbe915971f30ba
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-02 21:28:18 +0100

    Corrected the language code

--- scripts/Others/language.js
+++ scripts/Others/language.js
@@ -1,18 +1,16 @@
 const language = {
-    set current(name) {
-        this.log.push(name);
-    },
-    log: []
-}
+  log: [],
+
+  set current(name) {
+    this.log.push(name);
+  }
+};
 
 language.current = 'English';
-
 console.log(language.log); // ['English']
 
 language.current = 'French';
-
 console.log(language.log); // ['English', 'French']
 
 language.current = 'Spanish';
-
 console.log(language.log); // ['English', 'French', 'Spanish']
