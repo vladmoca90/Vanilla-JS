548482feeb0661c64f8932cb3f261b3705f553ec
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-02 21:22:37 +0100

    Corrected the switch with the week day

--- scripts/Switch/correct-day.js
+++ scripts/Switch/correct-day.js
@@ -1,23 +1,27 @@
-const correctDay = day =>{
-    switch (day) {
-        case "Monday":
-            console.log("This is the first day of the week!");
-            break;
-        case "Wednesday":
-            console.log("This is the third day of the week!");
-            break;
-        case "Sunday":
-            console.log("This is the last day of the week!");
-            break;
-        case "Tuesday":
-        case "Thursday":
-        case "Friday":
-        case "Saturday":
-            console.log("The day is not what you are looking for.");
-            break;
-        default:
-            throw new Error("The day is not a valid format.");
-    }
-}
+const describeDay = (day) => {
+  switch (day) {
+    case "Monday":
+      console.log("This is the first day of the week!");
+      break;
 
-correctDay("Sunday");
+    case "Wednesday":
+      console.log("This is the third day of the week!");
+      break;
+
+    case "Sunday":
+      console.log("This is the last day of the week!");
+      break;
+
+    case "Tuesday":
+    case "Thursday":
+    case "Friday":
+    case "Saturday":
+      console.log("The day is not what you are looking for.");
+      break;
+
+    default:
+      throw new Error("Invalid day. Please enter a valid weekday.");
+  }
+};
+
+describeDay("Sunday"); // This is the last day of the week!
