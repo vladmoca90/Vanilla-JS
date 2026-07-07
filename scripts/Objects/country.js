13292dc98a5422d8794b42006f8ce7db3eefb467
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-07 23:10:05 +0100

    Improved the country object

--- scripts/Objects/country.js
+++ scripts/Objects/country.js
@@ -1,16 +1,25 @@
-// A object with properties of a country
+// An object containing information about a country.
 
 const country = {
     name: 'United States',
-    population: 323, // in millions //
-    capital: 'Washington DC',
+    population: 323, // Population in millions
+    capital: 'Washington, D.C.',
     otherCities: ['Los Angeles', 'Chicago', 'Miami', 'Houston'],
-    size: 9826630, // in square km
+    size: 9826630, // Area in square kilometres
     region: 'North America',
     independence: 1776,
     landlocked: false,
-}
+};
 
-const countryProp = country => Object.keys(country);
-const countryValues = country => Object.values(country);
-const countryPairs = country => Object.entries(country);
+// Returns an array of the object's property names.
+const countryProp = (countryObj) => Object.keys(countryObj);
+
+// Returns an array of the object's values.
+const countryValues = (countryObj) => Object.values(countryObj);
+
+// Returns an array of [key, value] pairs.
+const countryPairs = (countryObj) => Object.entries(countryObj);
+
+console.log(countryProp(country));
+console.log(countryValues(country));
+console.log(countryPairs(country));
