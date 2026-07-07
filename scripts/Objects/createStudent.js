b9b939dc2a15b8523e6a3b10b0b09e08b122ee8a
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-07 23:11:51 +0100

    Corrected the student object

--- scripts/Objects/createStudent.js
+++ scripts/Objects/createStudent.js
@@ -1,28 +1,28 @@
-// A function that returns certain properties of an object in another object
+// A function that returns certain properties of an object in another object.
 
 const student = {
-    name: 'David James Ray',
-    degree: 'Bsc Computer Science',
-    university: 'University of Birmingham',
-    studentId: 1098085,
-    age: 16,
-    nationality: 'Canadian',
-    accommodation: 'Maple Bank',
+  name: 'David James Ray',
+  degree: 'BSc Computer Science',
+  university: 'University of Birmingham',
+  studentId: 1098085,
+  age: 16,
+  nationality: 'Canadian',
+  accommodation: 'Maple Bank',
 };
 
-const addStudentProp = student => {
-    let studentProp = Object.keys(student);
+const getStudentProperties = (student) => {
+  const studentProps = Object.keys(student);
 
-    if(studentProp.length == 0) {
-        throw new Error('The object cannot be empty');
-    }
+  if (studentProps.length === 0) {
+    throw new Error('The object cannot be empty.');
+  }
 
-    return Object.assign({
-        length: studentProp.length,
-        keys: studentProp,
-        entries: Object.entries(student),
-        values: Object.values(student),
-    });
-}
+  return {
+    length: studentProps.length,
+    keys: studentProps,
+    entries: Object.entries(student),
+    values: Object.values(student),
+  };
+};
 
-addStudentProp(student);
+console.log(getStudentProperties(student));
