6fababcaa6e14d302b12bf853a4925bca1078c00
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 16:05:36 +0100

    Corrected the function that returns a random number

--- scripts/Math/randomNumber.js
+++ scripts/Math/randomNumber.js
@@ -1,12 +1,13 @@
 // A function that returns a random number between a certain interval
 
 const randomNumber = (min, max) => {
-    if(min > max) {
-        throw new Error("The interval is invalid");
-    }
-    if(min == max) {
-        return min;
+    if (min > max) {
+        throw new Error("The interval is invalid.");
     }
 
-    return Math.floor(Math.random() * (max - min)) + min; // Math.random natively returns a number between 0 and 1
-}
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+//Example
+
+randomNumber(1, 5);
