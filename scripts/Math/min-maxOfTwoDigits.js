9c338a4d1b866099fd1c2d4f1a90368cd31c9890
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 16:08:30 +0100

    Corrected the function that returns the max and minimum number of two digits

--- scripts/Math/min-maxOfTwoDigits.js
+++ scripts/Math/min-maxOfTwoDigits.js
@@ -1,12 +1,21 @@
 // A function the returns the max and minimum number of two digits
 
 const numberOfTwo = (min, max) => {
-    let max = Math.ceil(max);
-    let min = Math.floor(min);
+    min = Math.floor(min);
+    max = Math.ceil(max);
 
-    if (min < 10 || max < 10 && min > 99 || max > 99) {
-        throw new Error('The number must be between 10 and 99 only');
+    if (min < 10 || max < 10 || min > 99 || max > 99) {
+        throw new Error('Numbers must be between 10 and 99.');
     }
 
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+    if (min > max) {
+        throw new Error('Minimum value cannot be greater than maximum value.');
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// Examples
+
+console.log(numberOfTwo(10, 99)); // 10–99
+console.log(numberOfTwo(25, 40)); // 25–40
