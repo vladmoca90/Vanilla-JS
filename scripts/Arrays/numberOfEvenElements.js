c615cda27aca93f9b43319070d7e9bae58013513
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 15:57:34 +0100

    Corrected the function wth the number of even elements

--- scripts/Arrays/numberOfEvenElements.js
+++ scripts/Arrays/numberOfEvenElements.js
@@ -1,20 +1,17 @@
 // A function that returns the number of even elements in an list
 
-const numberOfEvenElements = list => {
-    if (list.length == 0) {
-        return 0;
-    }
-    if (list.length == 1) {
-        return list[0] % 2 == 0 ? 1 : 0;
+const numberOfEvenElements = (list) => {
+    if (!Array.isArray(list)) {
+        throw new Error("Expected an array.");
     }
 
     let count = 0;
 
-    for (let i = 0; i < list.length; i++) {
-        if(list[i] % 2 == 0) {
+    for (const number of list) {
+        if (number % 2 === 0) {
             count++;
         }
     }
 
     return count;
-}
+};
