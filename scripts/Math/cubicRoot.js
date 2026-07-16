3f39ca8a1612eb5f12bfc86eb39eb117cefa7069
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 16:03:17 +0100

    IMproved the funciton that returns the cubic root

--- scripts/Math/cubicRoot.js
+++ scripts/Math/cubicRoot.js
@@ -1,3 +1,11 @@
 // A function that returns the cubic root of a number
 
-const returnCubicRoot = n => Math.cbrt(n);
+function returnCubeRoot(n) {
+  return Math.cbrt(n);
+}
+
+returnCubicRoot(27);    // 3
+returnCubicRoot(64);    // 4
+returnCubicRoot(0);     // 0
+returnCubicRoot(-8);    // -2
+returnCubicRoot(2);     // 1.2599210498948732
