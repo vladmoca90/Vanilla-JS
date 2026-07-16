b0521cee383d741fb88ef6725b3afcfcc038ea9d
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 16:12:09 +0100

    Corrected the class Animal {}

--- scripts/Classes/animal.js
+++ scripts/Classes/animal.js
@@ -6,15 +6,12 @@ class Animal {
 }
 
 class Tiger extends Animal {
-    constructor(family, species) {
-        super(family, species);
-    }
-
     get animalType() {
-        return this.family + ' ' + 'and' + ' ' + this.species;
+        return `${this.family} and ${this.species}`;
     }
 }
 
-const tiger = new Tiger('Felidae', 'Panthera onca');
+const tiger = new Tiger('Felidae', 'Panthera tigris');
 
-console.log(tiger.animalType); // Output will be 'Felidae and Panthere onca'
+console.log(tiger.animalType);
+// Felidae and Panthera tigris
