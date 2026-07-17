0cab31dfdbd18695221e49df4ce02b47c6df2e2e
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-18 00:03:04 +0100

    Fixed two more functions

--- scripts/Arrays/allElementsAreEven.js
+++ scripts/Arrays/allElementsAreEven.js
@@ -1,20 +1,19 @@
 // A function that returns true if all elements in an list are even and false otherwise
 
-const allElementsAreEven = list => {
-    if (list.length == 0) {
-        throw new Error("The list cannot be empty");
-    }
-    if (list.length == 1) {
-        if (list[0] % 2 !== 0) {
-            return false;
-        }
+const allElementsAreEven = (list) => {
+    if (!Array.isArray(list)) {
+        throw new TypeError("Expected an array");
     }
 
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] % 2 !== 0) {
+    if (list.length === 0) {
+        throw new Error("The list cannot be empty");
+    }
+
+    for (const num of list) {
+        if (num % 2 !== 0) {
             return false;
         }
     }
 
     return true;
-}
+};
--- scripts/Arrays/allEven-allOdd.js
+++ scripts/Arrays/allEven-allOdd.js
@@ -1,32 +1,7 @@
 // A function that returns true if all 3 elements are even
 
-//First case
-const allEven = list => {
-    let a = list[0];
-    let b = list[1];
-    let c = list[2];
+const allEven = ([a, b, c]) =>
+  a % 2 === 0 && b % 2 === 0 && c % 2 === 0;
 
-    if(a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
-        return true
-    } else {
-        return false;
-    }
-}
-
-//Second case
-const allEven = list => {
-    let a = list[0];
-    let b = list[1];
-    let c = list[2];
-
-    return a % 2 == 0 && b % 2 == 0 && c % 2 == 0;
-}
-
-//Return true is all numbers are odd
-const allOdd = list => {
-    let a = list[0];
-    let b = list[1];
-    let c = list[2];
-
-    return a % 2 !== 0 && b % 2 !== 0 && c % 2 !== 0;
-}
+const allOdd = ([a, b, c]) =>
+  a % 2 !== 0 && b % 2 !== 0 && c % 2 !== 0;
