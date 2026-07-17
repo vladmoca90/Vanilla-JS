2a72ee03a48c7141e4993c975e629883155ed3a6
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-18 00:05:45 +0100

    Fixed the function arrangeAscendingByLength

--- scripts/Arrays/arrangeAscendingByLength.js
+++ scripts/Arrays/arrangeAscendingByLength.js
@@ -1,23 +1,10 @@
 // A function that sorts the strings of an array by length from shortest to longest
 
-const sortAscByLength = list => {
-    if (list.length == 0) {
+// Sort strings by length from shortest to longest
+const sortAscByLength = (list) => {
+    if (list.length === 0) {
         throw new Error("The list cannot be empty");
     }
-    if (list.length == 1) {
-        return list[0];
-    }
 
-    let sortedList = [];
-
-    for (let i = 0; i < list.length; i++) {
-        if(list[i].length < list[i + 1].length) {
-            sortedList.push(list[i]);
-        }
-        if(list[i].length > list[i + 1].length) {
-            sortedList.push(list[i + 1]);
-        }
-    }
-
-    return sortedList;
-}
+    return [...list].sort((a, b) => a.length - b.length);
+};
