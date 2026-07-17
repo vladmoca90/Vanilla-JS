6c71f46e62ac2954007bb28fbaf604a5281644fd
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-18 00:18:21 +0100

    Fixed the JEST tets sample

--- scripts/JET-test.js
+++ scripts/JET-test.js
@@ -3,19 +3,25 @@
 //how many number 1s are in the following number 17272838119191929838299111
 //the result of 7 + 5
 
-function textUtilities() {
-    this.countLetters = function (text, letter) {
-        if (text.length == 0) {
-            return 0;
+class TextUtilities {
+    countLetters(text, letter, ignoreCase = true) {
+        if (text == null) {
+            throw new Error("Text cannot be null.");
         }
-        if (letter == null || letter.length == 0) {
-            throw new Error('The letter cannot be empty or cannot have a null value');
+
+        if (letter == null || letter.length !== 1) {
+            throw new Error("Please provide exactly one letter.");
+        }
+
+        if (ignoreCase) {
+            text = text.toLowerCase();
+            letter = letter.toLowerCase();
         }
 
         let count = 0;
 
-        for (let i = 0; i < text.length; i++) {
-            if (text[i] === letter) {
+        for (const character of text) {
+            if (character === letter) {
                 count++;
             }
         }
@@ -24,31 +30,61 @@ function textUtilities() {
     }
 }
 
-let textUtils = new textUtilities();
+class MathUtilities {
+    constructor(textUtilities = new TextUtilities()) {
+        this.textUtilities = textUtilities;
+    }
 
-console.log(textUtils.countLetters('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo.', 'e'));
-//returns 10
-
-function mathUtilities() {
-    this.add = function (a, b) {
+    add(a, b) {
         return a + b;
     }
 
-    this.countDigit = function (number, digit) {
-        if (number == null || digit == null) {
-            throw new Error('The number or digit cannot have a null value');
-        }
-        if (digit < 0 || digit > 9) {
-            throw new Error('The digit cannot be outside the interval of [0, 9]');
+    countDigit(number, digit) {
+        if (number == null) {
+            throw new Error("Number cannot be null.");
         }
 
-        let textUtils = new textUtilities();
+        if (!Number.isInteger(digit) || digit < 0 || digit > 9) {
+            throw new Error("Digit must be an integer between 0 and 9.");
+        }
 
-        return textUtils.countLetters(number.toString(), digit.toString());
+        return this.textUtilities.countLetters(
+            number.toString(),
+            digit.toString(),
+            false
+        );
     }
 }
 
-let mathUtils = new mathUtilities();
+// ----------------------
+// Testing
+// ----------------------
 
-console.log(mathUtils.countDigit(17271191919299111, 1)); //returns 5
-console.log(mathUtils.add(7, 5)); //returns 12
+const sentence =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eanean sodales justo et Enim ornare, a congue lacus commodo.";
+
+const number = 17272838119191929838299111;
+
+const textUtils = new TextUtilities();
+const mathUtils = new MathUtilities(textUtils);
+
+console.log("Count of 'e':",
+    textUtils.countLetters(sentence, "e")
+);
+
+console.log("Count of digit 1:",
+    mathUtils.countDigit(number, 1)
+);
+
+console.log("7 + 5 =",
+    mathUtils.add(7, 5)
+);
+
+// Additional tests
+
+console.log(textUtils.countLetters("eeee", "e"));       // 4
+console.log(textUtils.countLetters("EEEE", "e"));       // 4
+console.log(textUtils.countLetters("Hello", "l"));      // 2
+console.log(mathUtils.countDigit(111111, 1));           // 6
+console.log(mathUtils.countDigit(9876543210, 5));       // 1
+console.log(mathUtils.add(-10, 15));                    // 5
